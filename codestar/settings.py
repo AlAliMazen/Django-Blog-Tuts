90cf056f8d1cec3f39c4a903fca4f4aa076db5d1
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-06-18 07:46:01 +0200

    Set Debug to TRUE to see the problem of deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-12s_rh=keu0d395vezb$9uq^xl8gtvj77qkj2+(i2_+nmad*z@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
