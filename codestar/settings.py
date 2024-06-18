30dbc1046169d5e0cdc6ee37f1a7c999148f096a
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-06-18 06:35:10 +0200

    preparing the App for Herolu deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-12s_rh=keu0d395vezb$9uq^xl8gtvj77qkj2+(i2_+nmad*z@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
