6785c7895573efb6b8175c19f45714c72d0e5b65
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-06-22 17:19:03 +0200

    adding summernote to enhance superuser UX, learn how to load data from JSON file to populate the post database

--- codestar/urls.py
+++ codestar/urls.py
@@ -21,6 +21,8 @@ from django.urls import path, include
 
 urlpatterns = [
     #path('',view_blog.blog,name="blog"),
-    path("",include("blog.urls"),name="blog-urls"),
+    
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("",include("blog.urls"),name="blog-urls"),
 ]
