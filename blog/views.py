c25c35143bceedadeaac38a22e1b5b58e042b433
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-06-23 15:54:21 +0200

    understanding of queryset, genericView, pagination  and urls

--- blog/views.py
+++ blog/views.py
@@ -15,8 +15,12 @@ class PostList(generic.ListView):
     #queryset = Post.objects.all()
     #queryset = Post.objects.filter(author=2)
     # we can use another method like order_by
-    queryset = Post.objects.all().order_by("created_on") # if we want to order from most recent to oldest we use - sign before the created_on
+    #queryset = Post.objects.all().order_by("created_on") # if we want to order from most recent to oldest we use - sign before the created_on
 
     # showing only posts which are published 
-    queryset = Post.objects.filter(status=1) 
-    template_name = "post_list.html"
+    #queryset = Post.objects.filter(status=1) 
+    #template_name = "post_list.html"
+    # adding the index.html 
+    queryset = Post.objects.filter(status=1) # this line returns all the posts with all their attributes
+    template_name = "blog/index.html"
+    paginate_by = 6
