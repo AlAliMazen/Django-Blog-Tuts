458e4932614f80e8b40565a3e9c21832a6ad8f35
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-06-25 06:11:06 +0200

    creating new App about me, linking it all together and test it live

--- about/models.py
+++ about/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class About(models.Model):
+    """
+    for creating the About Me page 
+    """
+    title = models.CharField(max_length=200, unique=True)
+    content = models.TextField()
+    updated_on=models.DateTimeField(auto_now=True)
+
+
+     # Methods (functions inside classes) must always be written under the Meta
+    def __str__(self):
+        return f"{self.title} | written by {self.title}"
--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_summernote',
-    'blog'
+    'blog',
+    'about',
 ]
 
 MIDDLEWARE = [
